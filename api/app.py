3d797bacbe7bf9885397909005b0654c7c09a9e3
Michielvdz <r0750793@student.thomasmore.be>
2022-01-27 08:51:14 +0100

    okteto

--- api/app.py
+++ api/app.py
@@ -1,24 +1,28 @@
-# from fastai.vision import *
+from fastai.vision import *
 from flask import *
-import joblib
 
 app = Flask(__name__)
 
 @app.route('/result', methods=["POST"])
 def result():
-    # aipredict = load_learner('./laatstemodel.pkl')
-    model= joblib.load(open("laatstemodel.pkl", "rb"))
+    aipredict = load_learner('./laatstemodel.pkl')
     photo = request.files['file']
-    prediction = model.predict(photo)
+    prediction = aipredict.predict(photo)
 
     json_file = {}
     json_file['prediction'] = prediction
+    return jsonify(json_file)
+
+@app.route('/test')
+def test():
+    json_file = {}
+    json_file['hello'] = 'hello world'
     return jsonify(json_file)
 
 @app.route('/')
 def index():
   return "<h1>Welcome to the Plant AI</h1>"
 
-if __name__ == "__main__":
-  app.run()
+if __name__ == '__main__':
+  app.run(debug=True)
 
