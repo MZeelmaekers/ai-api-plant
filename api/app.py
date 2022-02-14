b078fe280519f685c2b04c66b96366666a7e455e
Michielvdz <r0750793@student.thomasmore.be>
2022-02-14 09:32:53 +0100

    pkl op azure

--- api/app.py
+++ api/app.py
@@ -2,49 +2,78 @@ import os
 from PIL import Image
 from fastai.vision.all import *
 from flask import *
-from flask_cors import CORS
+import pathlib
+from matplotlib import transforms
+from azure.storage.blob import BlobServiceClient, __version__
+from multiprocessing.pool import ThreadPool
 import pathlib
 
-from matplotlib import transforms
+
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
 
-app = Flask(__name__)
-CORS(app, resources={r"*": {"origins": "*"}})
 
-@app.route('/result', methods=["POST"])
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+container = "modelbotanic"
+localblobpath = "./"
+
+class AzureBlobFileDownloader():
+  def __init__(self, container):
+    self.blob_service_client =  BlobServiceClient.from_connection_string(connect_str)
+    self.my_container = self.blob_service_client.get_container_client(container)
+ 
+  def download_all_blobs_in_container(self):
+    my_blobs = self.my_container.list_blobs()
+    result = self.run(my_blobs)
+ 
+  def run(self,blobs):
+    with ThreadPool(processes=int(10)) as pool:
+     return pool.map(self.save_blob_locally, blobs)
+ 
+
+  def save_blob_locally(self, blob):
+    file_name = blob.name
+    bytes = self.my_container.get_blob_client(blob).download_blob().readall()
+ 
+    download_file_path = os.path.join(localblobpath, file_name)
+
+    os.makedirs(os.path.dirname(download_file_path), exist_ok=True)
+ 
+    with open(download_file_path, "wb") as file:
+      file.write(bytes)
+    return file_name
+
+
+app = Flask(__name__) 
+
+@app.route('/result', methods=["GET", "POST"])
 def result():
-    aipredict = load_learner('./beste.pkl')
-    photo = request.files["file"]
-    print('photo')
-    print(photo)
-    image = PILImage.create(photo)
-    print('image')
-    print(image)
-    
+
+  if not os.path.exists('./AI-model.pkl'):
+    azure_blob_file_downloader = AzureBlobFileDownloader(container)
+    azure_blob_file_downloader.download_all_blobs_in_container()
   
 
-    
 
-    prediction = aipredict.predict(image)
+  aipredict = load_learner('./AI-model.pkl')
+
+  photo = request.files["file"]
+  image = PILImage.create(photo)
+        
+  prediction = aipredict.predict(image)
 
 
-    week = prediction[0] 
-    accuracyprediction = re.search('TensorBase(.*)', str(max(prediction[2])))
-    accuracypr = accuracyprediction.group(1).strip('(').strip(')')
+  week = prediction[0] 
+  accuracyprediction = re.search('TensorBase(.*)', str(max(prediction[2])))
+  accuracypr = accuracyprediction.group(1).strip('(').strip(')')
 
 
-    json_file = {}
-    json_file['week'] = week
-    json_file['accuracy'] = accuracypr
+  json_file = {}
+  json_file['week'] = week
+  json_file['accuracy'] = accuracypr
 
-    return jsonify(json_file)
+  return jsonify(json_file)
 
-@app.route('/test')
-def test():
-    json_file = {}
-    json_file['hello'] = 'hello world'
-    return jsonify(json_file)
 
 @app.route('/')
 def index():
@@ -52,4 +81,11 @@ def index():
 
 if __name__ == '__main__':
   app.run(debug=True)
+
+
+
+
+
+
+
 
